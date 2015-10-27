9fe5cddbd5199a2a47a9e117d244ca6f7a59404d
- <appacademy@Apps-Mac-mini-36.local>
2015-10-27 18:05:21 -0400

    add individual route methods

--- lib/phase6/router.rb
+++ lib/phase6/router.rb
@@ -29,6 +29,7 @@ module Phase6
 
     # simply adds a new route to the list of routes
     def add_route(pattern, method, controller_class, action_name)
+      @routes << Route.new(pattern, method, controller_class, action_name)
     end
 
     # evaluate the proc in the context of the instance
@@ -39,6 +40,9 @@ module Phase6
     # make each of these methods that
     # when called add route
     [:get, :post, :put, :delete].each do |http_method|
+      define_method(http_method) do |pattern, c_class, action|
+        add_route(pattern, http_method, c_class, action)
+      end
     end
 
     # should return the route that matches this request
